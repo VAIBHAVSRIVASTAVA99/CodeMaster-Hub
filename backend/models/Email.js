2d0a23398840b9eb40b8e09d16dc87ee057c0f03
VAIBHAVSRIVASTAVA99 <srivastavavaibhav499@gmail.com>
2025-04-27 15:04:03 +0530

    deletingoldcode

--- backend/models/Email.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const emailSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-});
-
-module.exports = mongoose.model('Email', emailSchema);
--- frontend/Questify/src/config/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000',
-};
-
-// console.log('Backend URL:', import.meta.env.VITE_BACKEND_URL);
-
-export default config; 
